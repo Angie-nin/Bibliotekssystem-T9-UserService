b8058c45ad1b6ea3beddc10254b41aabaab28c96
Angelinne Fors <anfo0048@student.hv.se>
2026-03-12 21:17:08 +0100

    Add gitignore

--- obj/Debug/net10.0/UserService.AssemblyInfo.cs
+++ obj/Debug/net10.0/UserService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b4c9470e5419d987b2e14ca671fead330c5466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9196848fa993f93737e967ef220ebdd5e3f8a6f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
